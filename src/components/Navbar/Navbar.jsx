4b623f88fb9a89a68e2dd082e00a46449bad2258
Catarina Guimarães <catarina.guimaraes15@gmail.com>
2024-10-14 15:34:33 -0300

    feat: add Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../../../src/assets/images/logo.png";
+import "./style.css";
+
+export function Navbar() {
+    return (
+      <header>
+        <nav id="navbar">
+          <div className="nav-brand">
+            <Image className="nav-img" src={logo} alt="logo" />
+            <h1>PRO Lidera Skills</h1>
+          </div>
+
+          <ul className="nav-list">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Quem somos</a>
+            </li>
+            <li>
+              <a href="/">Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+
+export default Navbar;
