587f75059341f95a313520231931a9bd132923e8
leandrucarvalho <leandrucs@gmail.com>
2024-10-30 15:23:10 -0300

    refactor: ajustando página de login para ocupar espaço disponível

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,7 @@ import { Separator } from "@/components/ui/separator";
 
 export default function Login() {
   return (
-    <div className="flex mt-16">
+    <div className="flex mt-16 min-h-screen">
       <section className="hidden md:flex w-1/2 relative">
         <Image src={image.src} alt="Imagem de Login" fill />
       </section>
@@ -31,7 +31,7 @@ export default function Login() {
           />
           <Link href={"/instrucoes"}>
             <CustomButton
-              onClick={() => {}}
+              onClick={() => { }}
               className="text-white w-80 flex items-center justify-center gap-x-3 shadow-lg"
             >
               Entrar
@@ -51,31 +51,29 @@ export default function Login() {
 
           <CustomButton
             className="w-80 flex items-center justify-center gap-x-3 shadow-lg"
-            onClick={() => {}}
+            onClick={() => { }}
           >
             <FcGoogle /> Google
           </CustomButton>
           <CustomButton
             className="w-80 flex items-center justify-center gap-x-3 shadow-lg"
-            onClick={() => {}}
+            onClick={() => { }}
           >
             <FaFacebook className="text-[#1d2c4c]" /> Facebook
           </CustomButton>
-          <div className="flex items-center gap-3 justify-center mt-5">
-            <div className="flex items-center">
-              <h2 className="text-textColor text-center mr-2">
-                Novo no PRO <br /> Lidera Skills
-              </h2>
-              <div>
-                <Link href={"/cadastro"}>
-                  <CustomButton
-                    onClick={() => {}}
-                    className="text-primary w-fit bg-transparent hover:text-white"
-                  >
-                    CADASTRE-SE{" "}
-                  </CustomButton>
-                </Link>
-              </div>
+          <div className="flex items-center justify-center gap-x-3">
+            <h2 className="text-textColor text-center">
+              Novo no PRO Lidera Skills
+            </h2>
+            <div>
+              <Link href={"/cadastro"}>
+                <CustomButton
+                  onClick={() => { }}
+                  className="text-primary w-fit bg-transparent hover:text-white"
+                >
+                  CADASTRE-SE{" "}
+                </CustomButton>
+              </Link>
             </div>
           </div>
         </div>
