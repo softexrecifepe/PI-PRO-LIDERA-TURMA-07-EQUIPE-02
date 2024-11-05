776db8e101a0b917c46b0432b38c3edd7ed10e70
leandrucarvalho <leandrucs@gmail.com>
2024-11-05 20:07:08 -0300

    fix: envolver o componente ResultadoQuestionario em um componente Suspense para garantir que o suspense seja tratado adequadamente

--- src/app/resultado-questionario/page.tsx
+++ src/app/resultado-questionario/page.tsx
@@ -4,6 +4,7 @@ import image from "@/assets/images/img.result.png";
 import { Title } from "@/components/title";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function ResultadoQuestionario() {
   const searchParams = useSearchParams();
@@ -20,31 +21,33 @@ export default function ResultadoQuestionario() {
   const resultMessage = resultMessages[resultCategoryFromQuery as string] || "Resultado não encontrado.";
 
   return (
-    <div className="py-10 px-4 sm:px-8 md:py-24 flex flex-col items-center justify-center text-center w-full">
-      <Title />
-      <div className="text-black max-w-3xl flex flex-col gap-6 px-4 sm:px-8">
-        <h3 className="text-lg sm:text-xl font-semibold text-gray-500">
-          O resultado do seu teste é:
-        </h3>
+    <Suspense>
+      <div className="py-10 px-4 sm:px-8 md:py-24 flex flex-col items-center justify-center text-center w-full">
+        <Title />
+        <div className="text-black max-w-3xl flex flex-col gap-6 px-4 sm:px-8">
+          <h3 className="text-lg sm:text-xl font-semibold text-gray-500">
+            O resultado do seu teste é:
+          </h3>
 
-        <p className="text-base sm:text-lg font-semibold text-justify mb-8">
-          O tipo de
-          <span className="text-primary ml-2">{resultCategoryFromQuery}</span>. {resultMessage}
-        </p>
-        <div className="flex justify-center mb-8">
-          <Image
-            className="rounded max-w-full"
-            src={image.src}
-            alt="imagem do resultado"
-            width={500}
-            height={250}
-          />
-        </div>
-        <small className="text-sm text-gray-600">Clique no botão abaixo para baixar o resultado em PDF:</small>
-        <div className="flex justify-center items-center mt-4">
-          <CustomButton onClick={() => { }}>Download</CustomButton>
+          <p className="text-base sm:text-lg font-semibold text-justify mb-8">
+            O tipo de
+            <span className="text-primary ml-2">{resultCategoryFromQuery}</span>. {resultMessage}
+          </p>
+          <div className="flex justify-center mb-8">
+            <Image
+              className="rounded max-w-full"
+              src={image.src}
+              alt="imagem do resultado"
+              width={500}
+              height={250}
+            />
+          </div>
+          <small className="text-sm text-gray-600">Clique no botão abaixo para baixar o resultado em PDF:</small>
+          <div className="flex justify-center items-center mt-4">
+            <CustomButton onClick={() => { }}>Download</CustomButton>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
