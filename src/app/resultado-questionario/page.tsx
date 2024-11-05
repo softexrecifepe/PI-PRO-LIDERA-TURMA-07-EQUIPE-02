562020a15fe007257ae57c1d1dd1d06c3831b7be
Catarina Guimarães <catarina.guimaraes15@gmail.com>
2024-11-05 13:36:48 -0300

    feat: adiciona mensagem dinâmica de acordo com o resultado

--- src/app/resultado-questionario/page.tsx
+++ src/app/resultado-questionario/page.tsx
@@ -5,33 +5,31 @@ import { Title } from "@/components/title";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
-
 export default function ResultadoQuestionario() {
   const searchParams = useSearchParams();
   const resultCategoryFromQuery = searchParams.get('resultCategory');
 
+  // Objeto que mapeia categorias de resultado para mensagens dinâmicas
+  const resultMessages: { [key: string]: string } = {
+    "Liderança frágil e pouco trabalhada": "Você está apenas começando a desenvolver suas habilidades de liderança. É importante focar em autoconhecimento e na construção de relacionamentos dentro da equipe. Considere investir tempo em treinamentos e em feedbacks constantes.",
+    "Liderança em desenvolvimento": "Você está no caminho certo! Suas habilidades de liderança estão em crescimento e isso é um bom sinal. Continue a praticar a empatia e a comunicação clara, e busque oportunidades de liderança em projetos.",
+    "Líder de alta performance": "Parabéns! Você é um líder inspirador e eficaz. Sua capacidade de engajar sua equipe e promover um ambiente colaborativo é excepcional. Continue a buscar inovações e a desenvolver sua equipe para alcançar resultados ainda melhores.",
+  };
 
+  // Obtém a mensagem correspondente à categoria de resultado
+  const resultMessage = resultMessages[resultCategoryFromQuery as string] || "Resultado não encontrado.";
 
   return (
     <div className="py-10 px-4 sm:px-8 md:py-24 flex flex-col items-center justify-center text-center w-full">
       <Title />
       <div className="text-black max-w-3xl flex flex-col gap-6 px-4 sm:px-8">
-        <h2 className="flex justify-start font-bold text-lg sm:text-xl">Parabéns!</h2>
-
         <h3 className="text-lg sm:text-xl font-semibold text-gray-500">
           O resultado do seu teste é:
         </h3>
 
         <p className="text-base sm:text-lg font-semibold text-justify mb-8">
           O tipo de
-          <span className="text-primary ml-2">{resultCategoryFromQuery}</span>. Você
-          é um guia estratégico e emocional para sua equipe. Inspira pelo
-          exemplo, constrói um ambiente de trabalho saudável e está sempre em
-          busca de inovação e melhorias, garantindo que todos à sua volta
-          estejam engajados em um propósito maior e coletivo. Essa combinação de
-          habilidades permite que promova resultados excepcionais, que vão além
-          de metas e objetivos, alcançando um impacto profundo na vida dos
-          colaboradores e no sucesso da organização.
+          <span className="text-primary ml-2">{resultCategoryFromQuery}</span>. {resultMessage}
         </p>
         <div className="flex justify-center mb-8">
           <Image
