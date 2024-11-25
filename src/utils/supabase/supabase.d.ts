7ce99e4ac2d6707af23e5998cacdb317203a1eba
leandrucarvalho <leandrucs@gmail.com>
2024-11-25 15:55:45 -0300

    fix: resolvendo erro de build

--- src/utils/supabase/supabase.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "@/utils/supabase/server" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export function createClient(): Promise<any>; // ajuste o tipo conforme necessário
-}
