b187e080ea9f59dcb4933aa3194a53b904d77455
leandrucarvalho <leandrucs@gmail.com>
2024-12-02 10:27:13 -0300

    raw:  ajustando nomeclatura da chave do supabase

--- contexts/user-context.tsx
+++ contexts/user-context.tsx
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 interface UserContextType {
