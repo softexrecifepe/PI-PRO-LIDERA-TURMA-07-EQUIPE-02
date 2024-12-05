9a784231c0a624db4388da39f11b8e78d09f34a0
Catarina Guimarães <catarina.guimaraes15@gmail.com>
2024-12-05 20:33:11 -0300

    refactor: retira o 'testando Vercel' da página home na branch staging

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
 
         <div className="relative text-white text-justify w-11/12 sm:w-3/4 md:w-[50%] lg:w-[35%] flex flex-col justify-center items-center gap-8 p-4 md:p-0 top-1/3 -translate-y-full z-10">
           <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-[var(--primary-color)] mb-4 text-center">
-            Descubra seu potencial! TESTANDO VERCEL
+            Descubra seu potencial!
           </h2>
 
           <p className="text-white text-base sm:text-lg lg:text-xl font-semibold text-[var(--text-color)] mb-4 sm:mb-8 text-center">
